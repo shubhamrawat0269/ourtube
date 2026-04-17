be500faf52a5b3f8c918650cd02bff1fc5749138
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-17 22:37:36 +0530

    Added : Routes file created with testing also

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
 import express from "express";
-
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+import userRoutes from "./routes/user.route.js";
+
+app.use("/api/users", userRoutes);
 
 export default app;
