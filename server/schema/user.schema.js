945acc0eddd373fa886d1381d00b662e4d386630
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-17 22:23:41 +0530

    Added : User Model & Schema created

---
+++ server/schema/user.schema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {type: String, required: true},
+    channelName: {type: String, required: true},
+    phone: {type: String, required: true},
+    password: {type: String, required: true},
+    logoUrl: {type: String, required: true},
+    logoId: {type: String, required: true},
+    subscribers: {type: Number, default: 0},
+    subscribedChannels: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true },
+);
+
+export default userSchema;
