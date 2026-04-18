4f0a98372302aff4f62799a44d3ada38803b4d63
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-18 13:17:45 +0530

    Added : middleware to upload file added

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 import express from "express";
+import fileUpload from "express-fileupload";
 const app = express();
 
 import userRoutes from "./routes/user.route.js";
+
+// Middleware
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,6 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-router.post("/signup", (req, res) => res.json({ msg: "Users Works" }));
+router.post("/signup", (req, res) => {
+  // console.log(req.body);
+  console.log(req.files.logo);
+  res.json({ msg: "Users Works" });
+});
 
 export default router;
