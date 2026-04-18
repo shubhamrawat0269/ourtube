b188d9a9e0c67baa7e6db1e946a6dc9b179ef2eb
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-18 13:30:28 +0530

    Added : pwd hashing using bcryptjs

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,27 @@
 import express from "express";
+import bcrypt from "bcrypt";
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-  // console.log(req.body);
-  console.log(req.files.logo);
-  res.json({ msg: "Users Works" });
+router.post("/signup", async (req, res) => {
+  try {
+    // console.log(req.body);
+    // console.log(req.files.logo);
+    // res.json({ msg: "Users Works" });
+    const { password } = req.body;
+    const hashedPwd = await bcrypt.hash(password, 10);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+});
+
+router.post("/login", async (req, res) => {
+  const { password } = req.body;
+
+  const isMatchPwd = await bcrypt.compare(password, storedHash);
+  if(!isMatchPwd) return res.status(401).send("Invalid Cred")
+
+  res.send('Login success')
 });
 
 export default router;
