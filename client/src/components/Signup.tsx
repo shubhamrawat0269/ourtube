6800400c5599da50182209163b6c6601cc833a3d
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-25 10:38:06 +0530

    Added : Shadcn lib included in project

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default Signup;
